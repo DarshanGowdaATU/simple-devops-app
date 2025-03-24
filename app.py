bfd1997092d4b5607a013daad7b0641695a9a624
Darshan Gowda Anamanahalli Ramesh <l00187927@atu.ie>
2025-03-24 18:02:17 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+# app.py (Python example)
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    return "Hello from On-Demand EC2!"
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
